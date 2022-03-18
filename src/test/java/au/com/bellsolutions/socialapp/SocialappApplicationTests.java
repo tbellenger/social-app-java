0eedc8371ccfdd4566008cd3d9fb468cf2a9e71e
tbellenger <tbellenger@gmail.com>
2022-03-18 16:34:31 -0700

    initial commit

---
+++ src/test/java/au/com/bellsolutions/socialapp/SocialappApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.bellsolutions.socialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
