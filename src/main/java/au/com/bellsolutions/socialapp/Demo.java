f38f759c87fd13b468f3d89af0b331ae66d48cdf
tbellenger <tbellenger@gmail.com>
2022-03-19 08:59:27 -0700

    added models

---
+++ src/main/java/au/com/bellsolutions/socialapp/Demo.java
@@ -0,0 +1,50 @@
+package au.com.bellsolutions.socialapp;
+
+import java.util.Objects;
+
+public class Demo {
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Demo{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Demo demo = (Demo) o;
+        return getAge() == demo.getAge() && Objects.equals(getName(), demo.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    public Demo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/main/java/au/com/bellsolutions/socialapp/HelloWorld.java
@@ -0,0 +1,7 @@
+package au.com.bellsolutions.socialapp;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello hello");
+    }
+}
